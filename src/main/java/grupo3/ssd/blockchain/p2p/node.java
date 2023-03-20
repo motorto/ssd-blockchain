8920bd151afffcc5be470c83b3294f8660380a74
SaraRaquelSa <sara.raquel.25@hotmail.com>
2023-03-20 15:39:10 +0000

    node: start node kademlia

---
+++ src/main/java/grupo3/ssd/blockchain/p2p/node.java
@@ -0,0 +1,15 @@
+package grupo3.ssd.blockchain.p2p;
+
+import java.net.InetAddress;
+
+public class node {
+
+
+    // TODO: - node key
+    // TODO: - Tree<Buckets> (routing table) 
+    // TODO - Storage
+    
+    InetAddress ip_address;
+    int ip_port;
+
+}
