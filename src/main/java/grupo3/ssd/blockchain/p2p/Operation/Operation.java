b644f5affc850177279ecc00ec9db7974714e23b
André Cerqueira <acerqueira021@gmail.com>
2023-04-24 23:20:28 +0100

    Kademlia: Operation Interface

---
+++ src/main/java/grupo3/ssd/blockchain/p2p/Operation/Operation.java
@@ -0,0 +1,17 @@
+package grupo3.ssd.blockchain.p2p.Operation;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+public interface Operation {
+
+    /**
+     * An executor that runs the lookup operations on a timer to prevent problems when all first alpha nodes
+     */
+    static final ScheduledExecutorService scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
+
+    public void execute();
+
+    public boolean hasFinished();
+
+}
