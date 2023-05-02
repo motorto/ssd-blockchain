c51fd6c7c0993308a55033356a94e43339b39e14
André Cerqueira <acerqueira021@gmail.com>
2023-05-02 11:33:03 +0100

    App: Let's start from clean state with working Grpc.

--- src/main/java/group3/ssd/blockchain/App.java
+++ src/main/java/group3/ssd/blockchain/App.java
@@ -1,24 +1,10 @@
 package group3.ssd.blockchain;
 
-import group3.ssd.blockchain.p2p.Node.Identifier;
-import group3.ssd.blockchain.p2p.Node.Node;
-import group3.ssd.blockchain.p2p.Node.NodeTriplet;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) throws UnknownHostException {
-        Node a = new Node(new Identifier(), InetAddress.getByName("localhost"), 3000);
-
-        for (int i = 0; i < 100; i++) {
-            NodeTriplet tmp = new NodeTriplet(new Identifier(), InetAddress.getByName("localhost"), 3000, 0);
-            a.getRoutingTable().insert(tmp);
-        }
-
-        assert (a.getRoutingTable().getAllNodes().size() == 100);
+    public static void main(String[] args) {
+        System.out.println("HELLO");
     }
 }
--- src/main/java/group3/ssd/blockchain/p2p/NetworkStandards.java
+++ src/main/java/group3/ssd/blockchain/p2p/NetworkStandards.java
@@ -5,4 +5,6 @@ public class NetworkStandards {
     public final static int KEY_SIZE = 160;
     public static final int K_BUCKET_SIZE = 20;
 
+    public static final int MAX_CONCURRENT_CALLS = 3;
+
 }
