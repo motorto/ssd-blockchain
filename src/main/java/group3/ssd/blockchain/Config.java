6807bb65582c5dfc4369835094b7eeddd8c73907
André Cerqueira <acerqueira021@gmail.com>
2023-05-19 15:05:21 +0100

    P2P: add Config

---
+++ src/main/java/group3/ssd/blockchain/Config.java
@@ -0,0 +1,15 @@
+package group3.ssd.blockchain;
+
+public class Config {
+    public static final String COIN_NAME = "UnnamedCoin";
+    public static int MINING_DIFFICULTY = 2;
+    public static int NODE_CREATION_DIFFICULTY = 2;
+    public static final int MAX_NODES_BUCKET = 20;
+    public static final int ALPHA = 3;
+    public static String HASH_TYPE = "SHA-256";
+    public static int KEY_SIZE = 1024;
+    public static final int MAX_TRANSACTIONS_PER_BLOCK = 8;
+    public static final double MIN_NODE_REPUTATION = 0.7;
+    public static final int MINING_REWARD = 1;
+    public static String knownNode = "00161ec8fc86b769fea44a8216c9ce60cedfea5e8040e3ca3a4cd34ae17231d3";
+}
