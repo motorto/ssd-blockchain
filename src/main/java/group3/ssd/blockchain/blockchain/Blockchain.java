074134622c53c15f1e947c16b69662d911177870
brunasor <bruna.sor2000@gmail.com>
2023-05-20 19:15:23 +0100

    cenas

--- src/main/java/group3/ssd/blockchain/blockchain/Blockchain.java
+++ src/main/java/group3/ssd/blockchain/blockchain/Blockchain.java
@@ -19,7 +19,7 @@ public class Blockchain {
         createGenesisBlock(genesisWallet);
     }
 
-    // vazio
+    // criar vazio
     public Blockchain() throws InvalidKeySpecException, NoSuchAlgorithmException {
         chain = new ArrayList<>();
         pendingTransactions = new ArrayList<>();
@@ -32,6 +32,7 @@ public class Blockchain {
 
     }
 
+    // define transações pendentes e faz uma copia (evita referencias partilhadas)
     public void setPendingTransactions(ArrayList<Transaction> oldTransactionArrayList) {
         for (Transaction curTransaction : oldTransactionArrayList) {
             pendingTransactions.add(new Transaction(curTransaction.hash, curTransaction.senderPK, curTransaction.receiverPK, curTransaction.signature, curTransaction.timestamp, curTransaction.amount, curTransaction.misc));
@@ -42,6 +43,11 @@ public class Blockchain {
         return pendingTransactions;
     }
 
+    public int getPendingTransactionsLength() {
+        return pendingTransactions.size();
+    }
+
+    // define a cadeia de blocos com base numa lista antiga e faz copia
     public void setChain(ArrayList<Block> oldChain) {
         for (Block curBlock : oldChain) {
             ArrayList<Transaction> newTransactionList = new ArrayList<>();
@@ -56,6 +62,8 @@ public class Blockchain {
         return chain;
     }
 
+    // genesis: peça inicial da construção da blockchain
+    // inicializa com uma wallet e um valor 
     public void createGenesisBlock(Wallet genesisWallet) throws NoSuchAlgorithmException, SignatureException, InvalidKeySpecException, InvalidKeyException, UnsupportedEncodingException {
         Wallet tempWallet = new Wallet();
         this.addTransaction(tempWallet, genesisWallet, 100);
@@ -66,11 +74,9 @@ public class Blockchain {
         return chain.get(chain.size() - 1);
     }
 
-
     public boolean addBlock(Block newBlock) {
-
         if (!newBlock.verify()) {
-            System.out.println("Block add FAILED. The Hash didn't match with the existing contents.");
+            System.out.println("ERRO"); //hash nao corresponde
             return false;
         }
         int transactionSize = newBlock.getTransactionListSize();
@@ -79,23 +85,22 @@ public class Blockchain {
             if (pendingTransactions.size() == 0) {
                 return false;
             } else if (!pendingTransactions.get(i).equals(newBlock.transactionsList.get(i))) {
-                System.out.println("Block add FAILED. The Transactions in the block didn't match the pending ones.");
+                System.out.println("ERRO"); //a transação nao corresponde
                 pendingTransactions.get(i).printTransaction();
                 newBlock.transactionsList.get(i).printTransaction();
                 return false;
             }
-
         }
 
         pendingTransactions.subList(0, transactionSize).clear();
-
         newBlock.previousHash = this.getLatestBlock().hash;
 
         chain.add(newBlock);
-        System.out.println("Block add SUCCESSFULLY.");
+        System.out.println("ADICIONADO");
         return true;
     }
 
+    // verificar se blockchain é válida
     public boolean isValid() {
         Block cur, prev;
         String hashTarget = new String(new char[Config.MINING_DIFFICULTY]).replace('\0', '0');
@@ -104,78 +109,79 @@ public class Blockchain {
             cur = chain.get(i);
             prev = chain.get(i - 1);
 
-            //verify hash
+            //verifica hash
             if (!cur.hash.equals(cur.calculateHash())) {
                 return false;
             }
 
-            //verify parent
+            //verifica parente
             if (!cur.previousHash.equals(prev.hash)) {
                 return false;
             }
 
-            //verify if hash is mined
+            //verifica se a hash é mined
             if (!cur.hash.substring(0, Config.MINING_DIFFICULTY).equals(hashTarget)) {
                 return false;
             }
 
-            //verify transactions
+            //verifica transações
             for (int j = 0; j < cur.getTransactionListSize(); j++) {
                 Transaction curTransaction = cur.getTransactionList().get(j);
                 if (!curTransaction.hash.equals(curTransaction.calculateHash())) {
                     return false;
                 }
-                //verify signature
-
             }
         }
         return true;
 
     }
 
-    /**/
+    //imprime e diz se é valida
     public void printBlockChain() {
         System.out.println("blockchain:");
         for (Block currentBlock : chain) {
             currentBlock.printBlock();
         }
 
-        String isChainValid = this.isValid() ? "True" : "False";
-        System.out.println("\nThe Chain is valid: " + isChainValid);
+        String isCValid = this.isValid() ? "True" : "False";
+        if(isCValid.equals("False")){
+            System.out.println("Cadeia não é válida");
+        }
+        else{
+            System.out.println("Cadeia é válida");
+        }
     }
 
+    //Adiciona uma transação: quem envia, quem recebe e  valor
     public Transaction addTransaction(Wallet sender, Wallet receiver, int amount) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException, InvalidKeySpecException, UnsupportedEncodingException {
         Transaction newTransaction = new Transaction(sender.getPublicKey(), receiver.getPublicKey(), amount);
         newTransaction.signTransaction(sender);
 
         pendingTransactions.add(newTransaction);
-        System.out.println("Transaction of " + amount + " signed from " + sender.getPublicKey() + "\nto " + receiver.getPublicKey());
+        System.out.println("Valor " + amount + " enviado por " + sender.getPublicKey() + " para " + receiver.getPublicKey());
 
         return newTransaction;
     }
 
-    public int getPendingTransactionsLength() {
-        return pendingTransactions.size();
-    }
-
+    //Minera as transações pendentes num bloco novo
     public Block minePendingTransactions(Wallet miner) {
         int pendingTransactionsLength = getPendingTransactionsLength();
         if (pendingTransactionsLength == 0) {
-            System.out.println("There are no transactions pending to mine");
+            System.out.println("Não há transações pendentes");
             return null;
         }
-
+        
         ArrayList<Transaction> newBlockTransactions = new ArrayList<>();
         if (pendingTransactionsLength >= Config.MAX_TRANSACTIONS_PER_BLOCK) {
             for (int i = 0; i < Config.MAX_TRANSACTIONS_PER_BLOCK; i++) {
                 newBlockTransactions.add(pendingTransactions.get(i));
             }
         } else {
-            System.out.println("Not enough transactions to mine a full block (" + Config.MAX_TRANSACTIONS_PER_BLOCK + "), mining " + pendingTransactionsLength);
+            System.out.println("Não há transições suficientes (" + Config.MAX_TRANSACTIONS_PER_BLOCK + "), há apenas " + pendingTransactionsLength);
             newBlockTransactions.addAll(pendingTransactions);
         }
 
-
+        //criar novo bloco com as transações pendentes e adicona á cadeia
         Block newBlock;
         if (chain.size() == 0) {
             newBlock = new Block(0 + "", newBlockTransactions, "", User.wallet);
@@ -185,7 +191,7 @@ public class Blockchain {
         newBlock.mineBlock();
         chain.add(newBlock);
         pendingTransactions.subList(0, newBlockTransactions.size()).clear();
-        System.out.println("Mining Transaction Success!");
+        System.out.println("FEITO");
         return newBlock;
 
     }
